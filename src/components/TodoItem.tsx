3d4ff721c753cbf2a6196e472349b068e1e5a200
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-03-05 18:46:59 +0100

    feat: icoon check correct

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,5 @@
 import { CheckCircle2, Circle } from 'lucide-react'
+import { useState } from 'react';
 import '../assets/styles/components/todo-item.scss'
 import { iTodo } from '../interfaces/ITodo'
 
@@ -6,12 +7,31 @@ import { iTodo } from '../interfaces/ITodo'
 export default ({todo} : {todo: iTodo})  => {
     // TODO: i want props here
     // description, category, isDone, id
+    let iconStart = todo.isDone ? <CheckCircle2 className='c-todo-item__check c-todo-item__check--done'/> : <Circle className='c-todo-item__check c-todo-item__check--undone'/>;
+
+    const [icon, setIcon] = useState(iconStart);
     
+
+    const checkStatus = () => {
+        console.log('checkStatus')
+        todo.isDone = !todo.isDone
+        console.log(todo)
+        if (todo.isDone) {
+            setIcon(<CheckCircle2 className='c-todo-item__check c-todo-item__check--done'/>)
+        } else {
+            setIcon(<Circle className='c-todo-item__check c-todo-item__check--undone'/>)
+        }
+
+    }
+
     return(
         <li className="c-todo-item">
-            <input className='c-todo-item__input' type="checkbox" id='check1' checked={todo.isDone} readOnly/>
-            <label htmlFor="check1"><CheckCircle2/> <Circle/> {todo.name}</label>
-            <p>{todo.category}</p>
+            <input className='c-todo-item__input' type="checkbox" id={todo.id} onChange={checkStatus}/>
+            <label className='c-todo-item__label' htmlFor={todo.id}>{icon} </label>
+            <div className='c-todo-item__text'>
+                <label htmlFor={todo.id}>{todo.name}</label>
+                <p>{todo.category}</p>
+            </div>
         </li>
     )
 }
