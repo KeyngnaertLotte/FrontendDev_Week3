82fec5d85116170e578cd74c0733e311ceea560a
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-03-06 12:34:31 +0100

    Feat: Einde-les

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import './assets/styles/screen.scss'
+import AppFooter from './components/AppFooter';
+import Dashboard from './routes/Dashboard';
+import Settings from './routes/Settings';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard/>,
+  },
+  {
+    path: "/settings",
+    element: <Settings/>, // = lazy loading
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <div className="o-container">
+    <AppFooter/>
+    </div>
   </React.StrictMode>,
 )
